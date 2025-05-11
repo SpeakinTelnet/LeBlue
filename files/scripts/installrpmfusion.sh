54c0b4bf1c57585cbeb899f51f0641b3d4e79b7a
SpeakinTelnet <105018871+SpeakinTelnet@users.noreply.github.com>
2025-05-10 21:05:35 -0400

    feat: Create base functional template  (#1)
    * Create initial template
    
    * Add packages related to: sddm, hyprland, devops, and gaming
    
    * Add signing
    
    * Update README.md

---
+++ files/scripts/installrpmfusion.sh
@@ -0,0 +1,5 @@
+##!/usr/bin/env bash
+
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
