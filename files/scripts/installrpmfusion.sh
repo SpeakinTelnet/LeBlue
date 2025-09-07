2c2a9fd34f3f3a83cee2be75ed65b68a908c5cd8
SpeakinTelnet <gui.lac@protonmail.com>
2025-09-06 22:48:07 -0400

    Set retries to improve consistency

--- files/scripts/installrpmfusion.sh
+++ files/scripts/installrpmfusion.sh
@@ -1,5 +1,14 @@
-##!/usr/bin/env bash
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+RPMFUSION_FREE="https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm"
+RPMFUSION_NONFREE=https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
+
+
+curl --retry 3 --retry-delay 5 -LO "${RPMFUSION_FREE}"
+curl --retry 3 --retry-delay 5 -LO "${RPMFUSION_NONFREE}"
 
 rpm-ostree install \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
+    "rpmfusion-free-release-${OS_VERSION}.noarch.rpm" \
+    "rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
